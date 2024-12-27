0cd0a0e0385e6182eee2d3e7b52a827f2c84a238
Zahed75 <safayatofficial1@gmail.com>
2024-12-27 14:17:52 +0600

    mongoose connection update

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -3,21 +3,32 @@ const mongoose = require("mongoose");
 
 // create a connect
 const uri = process.env.MONGODB_URI;
-
 const connectWithDB = async () => {
   try {
-    const db = await mongoose.connect(uri);
+    const db = await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, // Wait 10 seconds for server selection
+      socketTimeoutMS: 45000,         // Close sockets after 45 seconds of inactivity
+    });
     console.log("Connected to Database");
     return db;
   } catch (e) {
-    console.log(e);
+    console.error("Failed to connect to the database. Exiting now...");
+    console.error(e.message);
     process.exit(1);
   }
 };
 
-const disconnectFromDB = () => {
-  return mongoose.connection.close();
+const disconnectFromDB = async () => {
+  try {
+    await mongoose.connection.close();
+    console.log("Disconnected from Database");
+  } catch (e) {
+    console.error("Error disconnecting from the database:", e.message);
+  }
 };
+
 
 // export them
 module.exports = {
